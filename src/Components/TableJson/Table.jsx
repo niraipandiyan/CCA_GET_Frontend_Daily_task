ad5ec254a38086a5a82e4ea200a10b19e412ff77
niraipandiyan <nirai.pandiyan@indiumsoft.com>
2024-04-04 11:12:40 +0530

    Filter on cpu,disk,memory

--- src/Components/TableJson/Table.jsx
+++ src/Components/TableJson/Table.jsx
@@ -15,7 +15,7 @@ import { styled } from "@mui/system";
 // Styled components for custom styling
 const StyledTableContainer = styled(TableContainer)({
   width: "100%",
-  height: "80%",
+  height: "500px",
   overflow: "auto",
   boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
   borderRadius: "8px",
@@ -45,32 +45,28 @@ const StyledTableRow = styled(TableRow)({
 });
 
 export default function Tab() {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const groupedData = details.reduce((acc, curr) => {
+    if (!acc[curr.instance_id]) {
+      acc[curr.instance_id] = {
+        instance_id: curr.instance_id,
+        region: curr.region,
+        instance_name: curr.instance_name,
+        instance_type: curr.instance_type,
+        state: curr.state,
+        last_activity_time: curr.last_activity_time,
+      };
+    }
+    acc[curr.instance_id][curr.metric_type] = curr.average_utilization;
+    return acc;
+  }, {});
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    console.log("Page:", page);
-    console.log("Rows per page:", rowsPerPage);
-    console.log(
-      "Sliced details:",
-      details.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-    );
-    setPage(0);
-  };
+  const rowData = Object.values(groupedData);
 
   return (
     <div style={{ display: "flex" }}>
-      <div style={{ height: "500px", width: "1200px" }}>
+      <div>
         <div>
-          <StyledTableContainer
-            style={{ margin: "auto", marginTop: "10px" }}
-            component={Paper}
-          >
+          <StyledTableContainer style={{ margin: "auto" }} component={Paper}>
             <Table>
               <TableHead>
                 <TableRow>
@@ -79,138 +75,121 @@ export default function Tab() {
                   <StyledTableCell>Instance ID</StyledTableCell>
                   <StyledTableCell>Instance Type</StyledTableCell>
                   <StyledTableCell>State</StyledTableCell>
-                  <StyledTableCell>Metric Type</StyledTableCell>
-                  <StyledTableCell>Average Utilization</StyledTableCell>
+                  <StyledTableCell>CPU</StyledTableCell>
+                  <StyledTableCell>Disk</StyledTableCell>
+                  <StyledTableCell>Memory</StyledTableCell>
                   <StyledTableCell>Last Activity Time</StyledTableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {details
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row) => (
-                    <StyledTableRow
-                      key={
-                        row.last_activity_time +
-                        row.instance_id +
-                        row.instance_name +
-                        row.average_utilization
-                      }
+                {rowData.map((row) => (
+                  <StyledTableRow
+                    key={
+                      row.last_activity_time +
+                      row.instance_id +
+                      row.instance_name +
+                      row.average_utilization
+                    }
+                  >
+                    <TableCell
+                      style={{
+                        border: "1px solid #DDDDDD",
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                        whiteSpace: "nowrap", // Prevent line breaks within the cell
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
                     >
-                      <TableCell
-                        style={{
-                          border: "1px solid #DDDDDD",
-                          fontFamily: "monospace",
-                          boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
-                          whiteSpace: "nowrap", // Prevent line breaks within the cell
-                          overflow: "hidden",
-                          textOverflow: "ellipsis",
-                        }}
-                      >
-                        {row.region}
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          border: "1px solid #DDDDDD",
-                          fontFamily: "monospace",
-                          boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
-                        }}
-                      >
-                        {row.instance_name !== null
-                          ? row.instance_name
-                          : "Not Available"}
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          fontFamily: "monospace",
-                          boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
-                          whiteSpace: "nowrap", // Prevent line breaks within the cell
-                          overflow: "hidden",
-                          textOverflow: "ellipsis",
-                        }}
-                      >
-                        {row.instance_id}
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          fontFamily: "monospace",
-                          boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
-                        }}
-                      >
-                        {row.instance_type}
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          fontFamily: "monospace",
-                          boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
-                          fontWeight: "bolder",
-                          fontSize: "0.75rem",
-                          color: "black",
-                          backgroundColor:
-                            (row.state === "running" && "#7db58e") ||
-                            (row.state === "stopped" && "#D37676"),
-                          borderRadius: 8,
-                          padding: "3px 5px",
-                          marginTop: "25px",
-                          marginLeft: "8px",
-                          display: "inline-block",
-                        }}
-                      >
-                        {row.state}
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          fontFamily: "monospace",
-                          boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
-                        }}
-                      >
-                        {row.metric_type}
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          fontFamily: "monospace",
-                          boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
-                        }}
-                      >
-                        {row.average_utilization !== null
-                          ? row.average_utilization
-                          : "NA"}
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          fontFamily: "monospace",
-                          boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
-                        }}
-                      >
-                        {row.last_activity_time !== null
-                          ? row.last_activity_time
-                          : "NA"}
-                      </TableCell>
-                    </StyledTableRow>
-                  ))}
+                      {row.region}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        border: "1px solid #DDDDDD",
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                      }}
+                    >
+                      {row.instance_name !== null
+                        ? row.instance_name
+                        : "Not Available"}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                        whiteSpace: "nowrap", // Prevent line breaks within the cell
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      {row.instance_id}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                      }}
+                    >
+                      {row.instance_type}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                        fontWeight: "bolder",
+                        fontSize: "0.75rem",
+                        color: "black",
+                        backgroundColor:
+                          (row.state === "running" && "#7db58e") ||
+                          (row.state === "stopped" && "#D37676"),
+                        borderRadius: 8,
+                        padding: "3px 5px",
+                        marginTop: "25px",
+                        marginLeft: "8px",
+                        display: "inline-block",
+                      }}
+                    >
+                      {row.state}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                      }}
+                    >
+                      {row.cpu || "NA"}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                      }}
+                    >
+                      {row.disk || "NA"}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                      }}
+                    >
+                      {row.memory || "NA"}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        fontFamily: "monospace",
+                        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)",
+                      }}
+                    >
+                      {row.last_activity_time !== null
+                        ? row.last_activity_time
+                        : "NA"}
+                    </TableCell>
+                  </StyledTableRow>
+                ))}
               </TableBody>
             </Table>
-            <TablePagination
-              sx={{
-                backgroundColor: "#B3C8CF",
-                border: "1px solid #BED7DC",
-                fontFamily: "monospace",
-              }}
-              rowsPerPageOptions={[5, 10, 15]}
-              component="div"
-              count={details.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              slotProps={{
-                select: {
-                  style: { fontFamily: "monospace", fontWeight: "bold" },
-                },
-                actions: {
-                  style: { fontFamily: "monospace" },
-                },
-              }}
-            />
           </StyledTableContainer>
         </div>
       </div>
