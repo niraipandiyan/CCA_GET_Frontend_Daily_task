5a968da8d71378e221160e331cf4006d580c16f9
niraipandiyan <nirai.pandiyan@indiumsoft.com>
2024-04-09 17:46:02 +0530

    Responsive chart

--- src/Components/Charts/Fetch.jsx
+++ src/Components/Charts/Fetch.jsx
@@ -9,8 +9,8 @@ import {
   Tooltip,
   Rectangle,
 } from "recharts";
-import CustomizedMenus from "./Dropdown";
 
+import { Box } from "@mui/material";
 
 const DataComponent = () => {
   const [data, setData] = useState("");
@@ -22,7 +22,6 @@ const DataComponent = () => {
           "https://mocki.io/v1/ce654693-2ba0-4528-b9f5-6d2d61be1bb4"
         );
         const result = await response.json();
-        console.log(result);
         setData(result);
       } catch (error) {
         console.error("Error fetching data: ", error);
@@ -33,69 +32,60 @@ const DataComponent = () => {
   }, []);
 
   return (
-    <ResponsiveContainer
-      // className={classes.rechartsWrapper}
-      width="100%"
-      height="150%"
-      aspect={3}
-      // Optimized aspect ratio for better responsiveness
-      resizeMode="container"
-      // Resized based on container dimensions
-      style={{
-        padding: "20px",
-        paddingBottom: "35px",
-        borderRadius: "4px",
-        boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.2)",
+    <Box
+      sx={{
+        width: { xs: "75%", sm: "400px", md: "600px", lg: "800px" },
+        height: { xs: "150px", sm: "180px", md: "260px", lg: "300px" },
+        margin: "auto",
+        padding: "5px",
       }}
     >
-      <CustomizedMenus />
-      <BarChart
-        data={data}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-        style={{ margin: "10px", marginLeft: "0px" }}
-      >
-        <defs>
-          <linearGradient
-            id="colorUv"
-            x1="0"
-            y1="0"
-            x2="0"
-            y2="100%"
-            spreadMethod="reflect"
-          >
-            <stop offset="0" stopColor="#CB5EDC" />
-            <stop offset="1" stopColor="#278EF1" />
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="service" tickLine={false} axisLine={false} />
-        <YAxis
-          dataKey="Cost"
-          tickLine={false}
+      <ResponsiveContainer width="100%" height="100%">
+        <h2 style={{ textAlign: "center", background: "linear-gradient(90deg, #CB5EDC 40%, #278EF1 60%)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent"}}>Cost spent</h2>
+        <BarChart
+          data={data}
+        >
+          <defs>
+            <linearGradient
+              id="colorUv"
+              x1="0"
+              y1="0"
+              x2="0"
+              y2="100%"
+              spreadMethod="reflect"
+            >
+              <stop offset="0" stopColor="#CB5EDC" />
+              <stop offset="1" stopColor="#278EF1" />
+            </linearGradient>
+          </defs>
+          <XAxis 
+          dataKey="service" 
+          tickLine={false} 
           axisLine={false}
-          style={{ fontWeight: "bolder" }}
-        />
-        <CartesianGrid vertical={false} fill="none" />
-        <Tooltip />
-
-        <Bar
-          on
-          barSize={18}
-          onMouseEnter={false}
-          radius={3}
-          type="monotone"
-          dataKey="Cost"
-          fill="#4584ED"
-          stackId="1"
-          activeBar={<Rectangle fill="url(#colorUv)" />}
-        />
-      </BarChart>
-
-    </ResponsiveContainer>
+          interval={0}
+          style={{ fontFamily:"monospace",fontSize: 10 }} />
+          <YAxis
+            dataKey="Cost"
+            tickLine={false}
+            axisLine={false}
+            style={{ fontWeight: "bolder", fontFamily:"monospace" }}
+          />
+          <CartesianGrid vertical={false} fill="none" />
+          <Tooltip />
+          <Bar
+            on
+            barSize={25}
+            onMouseEnter={false}
+            radius={3}
+            type="monotone"
+            dataKey="Cost"
+            fill="url(#colorUv)"
+            stackId="1"
+            activeBar={<Rectangle fill="#4584ED" />}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </Box>
   );
 };
 
